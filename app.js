d924972d310261f80b65321e7ec617e1d3df0368
Kristi Fidani <kristifidani@hotmail.com>
2021-06-08 11:06:12 +0200

    Heroku deployment setup

--- app.js
+++ app.js
@@ -113,10 +113,7 @@ app.get("/", function(req, res) {
 });
 
 //For Heroku deployment
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+let port = process.env.PORT || 3000;
 //Start Server
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
