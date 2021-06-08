dbfc14b393dda237e030cf9f00ffff9772e994c4
Kristi Fidani <kristifidani@hotmail.com>
2021-06-08 12:53:25 +0200

    Heroku deplyoment setup

--- app.js
+++ app.js
@@ -114,6 +114,6 @@ app.get("/", function(req, res) {
 
 
 //Start Server
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started successfully!`);
 });
